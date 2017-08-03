c4b2bddddd54f2a577e76e58c3122a4ecb09d24f
BartPu <bpuchalski@tlen.pl>
2017-08-03 14:45:23 +0200

    commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+alert('Hello, ' + name); // here's my comment
+console.log('Hello, ' + name); /* here's another one */
+
+
+// I can comment all day long
+/* it's easy and
+simple */
